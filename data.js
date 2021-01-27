a1f3ad50f9f6be3f9d850fd20922c7961ce304e5
JasonFerrrari <jason.ferrari@uconn.edu>
2021-01-26 20:51:49 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-37dba7cc-24a1-4c25-a7af-5c44d201d144",
+      "name": "37DBA7CC-24A1-4C25-A7AF-5C44D201D144",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.1887696911801484,
+        "pitch": 0.04093025164492303,
+        "fov": 1.3445094916099283
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.2607336130970346,
+          "pitch": 1.4804009250915353,
+          "title": "Post Processing",
+          "text": "Having a tech background, I am able to perform post processing on images created. Customizing the experience to your liking."
+        },
+        {
+          "yaw": -0.8011262302482365,
+          "pitch": -0.04865695243845991,
+          "title": "HDR Optimization",
+          "text": "Manually altering settings integrated with HDR Optimization, images come out to the highest degree of quality."
+        },
+        {
+          "yaw": 0.7579254696481481,
+          "pitch": -0.47446052366352554,
+          "title": "Embedded Content",
+          "text": "Adding embedded content gives you the ability to add your own personal touch to every tour. Logos, 2D Images and even videos can be integrated to add to the immersive experience."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
